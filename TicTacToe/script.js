ad33a0c2ed994c4845e74b361c5ed10986b22cdc
SulaimanMj <s.m.j.7@hotmail.com>
2023-10-10 20:32:11 +0300

    JS 2 Exercise 2B
    Made the game playable using Keyboard with a pop-up message explaining how to use it.

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,3 +1,5 @@
+alert("Welcome to Tic Tac Toe! In order to Play using the Arrows please Click TAB button and then use the arrows to Hover over the cells and Enter or Space to select one.");
+
 const cells = document.querySelectorAll('.cell');
 let currentPlayer = 'X';
 let board = Array(9).fill(null);
@@ -25,7 +27,6 @@ function checkWinner() {
         }
     }
 
-    
     if (!board.includes(null)) {
         setTimeout(() => {
             alert('Draw!');
@@ -40,7 +41,7 @@ function makeMove(index) {
     if (!board[index]) {
         board[index] = currentPlayer;
         cells[index].innerText = currentPlayer;
-        cells[index].classList.add(currentPlayer);  
+        cells[index].classList.add(currentPlayer);
         if (!checkWinner()) {
             currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
         }
@@ -55,3 +56,20 @@ function resetGame() {
     });
     currentPlayer = 'X';
 }
+
+cells.forEach((cell, index) => {
+    cell.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter' || event.key === ' ') {
+            event.preventDefault();
+            makeMove(index);
+        } else if (event.key === 'ArrowRight' && index % 3 < 2) {
+            cells[index + 1].focus();
+        } else if (event.key === 'ArrowLeft' && index % 3 > 0) {
+            cells[index - 1].focus();
+        } else if (event.key === 'ArrowDown' && index < 6) {
+            cells[index + 3].focus();
+        } else if (event.key === 'ArrowUp' && index > 2) {
+            cells[index - 3].focus();
+        }
+    });
+});
